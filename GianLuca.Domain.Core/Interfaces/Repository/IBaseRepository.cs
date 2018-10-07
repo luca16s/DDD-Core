530e8d3df0714a20c60eb26349b13b002fbe1173
luca16s <luca.gian16@outlook.com>
2018-10-07 15:33:52 -0300

    Creation of BaseRepository and a BaseRepository for asynchronous.

---
+++ GianLuca.Domain.Core/Interfaces/Repository/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GianLuca.Domain.Core.Entity;
+
+namespace GianLuca.Domain.Core.Interfaces.Repository
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        T AddItem(T item);
+        T GetItem(Guid id);
+        IEnumerable<T> GetAllItems();
+        T UpdateItem(Guid id, T item);
+        void DeleteItem(T item);
+    }
+}
---
+++ GianLuca.Domain.Core/Interfaces/Repository/IBaseRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GianLuca.Domain.Core.Entity;
+
+namespace GianLuca.Domain.Core.Interfaces.Repository
+{
+    public interface IBaseRepositoryAsync<T> where T : BaseEntity
+    {
+        Task<T> AddItemAsync(T item);
+        Task<T> GetItemAsync(Guid id);
+        Task<IEnumerable<T>> GetAllItemsAsync();
+        T UpdateItem(Guid id, T item);
+        void DeleteItem(T item);
+    }
+}
