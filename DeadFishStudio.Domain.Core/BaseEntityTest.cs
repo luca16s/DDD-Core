e828d3ed8a1ccee6ce89adbdd5d0f73e1eaa817a
luca16s <luca.gian16@outlook.com>
2018-10-21 20:14:08 -0300

    Added tests

---
+++ DeadFishStudio.Domain.Core/BaseEntityTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GianLuca.Domain.Core.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GianLuca.Domain.Core.UnitTest
+{
+    [TestClass]
+    public class BaseEntityTest
+    {
+        [TestMethod]
+        public void GenerateGuid()
+        {
+            var entity = new BaseEntity();
+
+            Assert.IsNotNull(entity);
+        }
+    } 
+}
