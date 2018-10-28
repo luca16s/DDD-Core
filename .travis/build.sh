021dc535040d8625356b7de6a8b2df8d78a3a17b
luca16s <luca.gian16@outlook.com>
2018-10-27 22:34:38 -0300

    Modified build and test files.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd ../GianLuca.Domain.Core
+cd ../GianLuca.Domain.Core/
 dotnet restore
 dotnet build
--- .travis/test.sh
+++ .travis/test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-cd ../GianLuca.Domain.Core.UnitTest
+cd ../GianLuca.Domain.Core.UnitTest/
+dotnet restore
 dotnet test
