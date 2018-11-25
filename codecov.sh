203bad027245197de1e35034b7b2954e784e3eaa
luca16s <luca.gian16@outlook.com>
2018-11-25 12:42:17 -0200

    Inclusion of Test Coverage Tool

--- codecov.sh
+++ codecov.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl -s https://codecov.io/bash > codecov
 chmod +x codecov
-./codecov -f "DomainCore_coverage.xml" -t CODECOV_TOKEN
+./codecov -f "coverage.opencover.xml" -t CODECOV_TOKEN
