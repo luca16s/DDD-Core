35d31825107a697f7b1b66fd9152bf45fb353df2
Gian Luca <luca.gian16@outlook.com>
2019-10-05 20:18:06 -0300

    Criada a baseViewModel

---
+++ GianLuca.Domain.Core/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="BaseViewModel.cs" company="Gian Luca da Silva Figueiredo">
+// Copyright (c) Gian Luca da Silva Figueiredo. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace GianLuca.Domain.Core.ViewModel
+{
+    using System;
+
+    /// <summary>
+    /// ViewModelBase base.
+    /// </summary>
+    public class BaseViewModel
+    {
+        /// <summary>
+        /// Obtém identificador da ViewModel.
+        /// </summary>
+        public Guid Id { get; } = Guid.NewGuid();
+    }
+}
