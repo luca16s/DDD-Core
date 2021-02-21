32e13a203271054d829d4b545a963298a004d011
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-02-21 19:26:31 -0300

    [06]
    - Incluir o humanizer
    - Incluir FluentValidations
    - Mudar esquema de versionamento

---
+++ DeadFishStudio.CoreLibrary/Validations/BaseEntityValidations.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="BaseEntityValidations.cs" company="DeadFish Studio">
+// Copyright (c) DeadFish Studio. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace DeadFishStudio.CoreLibrary.Validations
+{
+    using FluentValidation;
+
+    public class BaseEntityValidations : AbstractValidator<BaseEntity>
+    {
+        public BaseEntityValidations()
+        {
+            RuleFor(baseEntity => baseEntity.Id)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
