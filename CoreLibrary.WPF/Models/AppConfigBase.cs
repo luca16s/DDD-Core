f61915544af67875010e1111bfe50cffc63a03d9
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-04-15 20:43:01 -0300

    - Criar classe exclusiva para Secrets

--- CoreLibrary.WPF/Models/AppConfigBase.cs
+++ CoreLibrary.WPF/Models/AppConfigBase.cs
@@ -13,15 +13,4 @@
         public Secret SelectedSecret => /*Dropbox ?? */OneDrive;
         public string UserFileName { get; set; }
     }
-
-    public class Secret
-    {
-        public string AppKey { get; set; }
-        public string AppSecret { get; set; }
-        public string AppToken { get; set; }
-        public string Endpoint { get; set; }
-        public string JsRedirectEntpoint { get; set; }
-        public string RedirectEndpoint { get; set; }
-        public string Service { get; set; }
-    }
 }
