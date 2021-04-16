6acd397dee12178214d94eb8d5c1d0df3770b5ca
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-04-15 21:41:56 -0300

    - Incluir testes

---
+++ CoreLibrary.UnitTest/EnumModelTests.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+
+using CoreLibrary.Models;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace CoreLibrary.UnitTest
+{
+    public class EnumModelTests
+    {
+        [Fact]
+        public void EnumDescriptionShouldNotReturnNull()
+        {
+            var enumModelo = new Faker<EnumModel>()
+                .RuleFor(g => g.Description, f => f.Lorem.Word())
+                .Generate();
+
+            _ = enumModelo.Description.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void EnumModelShouldNotBeNull()
+        {
+            var enumModelo = new Faker<EnumModel>()
+                .RuleFor(g => g.Description, f => f.Lorem.Word())
+                .Generate();
+
+            _ = enumModelo.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void EnumValueShouldNotReturnNull()
+        {
+            var enumModelo = new Faker<EnumModel>()
+                .RuleFor(g => g.Value, f => EError.TESTE1)
+                .Generate();
+
+            _ = enumModelo.Value.Should().NotBeNull();
+        }
+    }
+}
