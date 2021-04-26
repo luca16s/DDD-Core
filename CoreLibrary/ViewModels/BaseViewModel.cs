b24415b6f68c0928f9fda00f2119e10fb888ae21
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-04-26 06:43:51 -0300

    - Incluir viewModelBase

---
+++ CoreLibrary/ViewModels/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreLibrary.ViewModels
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using System;
+
+    public class BaseViewModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public Guid Id { get; set; }
+    }
+}
